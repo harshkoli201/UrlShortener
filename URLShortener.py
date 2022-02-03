2cca5f0add7db02066dcf8025467e9438e1187e7
Harsh Koli <harshkoli201@gmail.com>
2022-02-03 12:02:53 +0530

    Done Getting Url

--- URLShortener.py
+++ URLShortener.py
@@ -1,10 +1,30 @@
+from ast import Interactive
+from cgitb import text
+from turtle import width
 import pyshorteners
+from tkinter import *
 
-url = input("Enter the URL to shorten: ")
+app = Tk()
+app.title("UrlShortener")
+app.geometry('500x500')
 
-typeurl = pyshorteners.Shortener(api_key='a489c6c0fd716e34ae803a957aa37ce1d192cf72')
-short = typeurl.bitly.short(url)
+
+def short(): 
+        # sloved by using l = url.get() instead of l = Str(url.get())
+
+        l = url.get()
+        typeurl = pyshorteners.Shortener(api_key='a489c6c0fd716e34ae803a957aa37ce1d192cf72')
+        short = typeurl.bitly.short(l)
+        ans.config(text="The Shortened URL is: " + short)
 
 
-print("The Shortened URL is: " + short)
+Lab = Label(app,text = "Enter the URL to shorten:",font='Helvetica 10 bold').place(x = 150,y = 20)  
 
+url = Entry(app, width=40)
+url.pack(padx=40,pady=60)
+
+button = Button(app, text="Click to Short URL", command=short, bg="gray").place(x = 195,y = 150)  
+
+ans = Label(app,relief='flat',text='',font='Helvetica 12 bold')
+ans.pack(padx=50,pady=60)
+app.mainloop()
